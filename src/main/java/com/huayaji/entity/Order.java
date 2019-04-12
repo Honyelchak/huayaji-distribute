76b34694116e5495109e413a79dbd8c8df022c3f
Honyelchak <554417388@qq.com>
2019-04-12 10:56:02 +0800

    1.更新后台管理模板

--- src/main/java/com/huayaji/entity/Order.java
+++ src/main/java/com/huayaji/entity/Order.java
@@ -10,7 +10,10 @@ public class Order implements Serializable {
 
     @Id
     private Long id;
-    private String user_id;
+
+    @ManyToOne()
+    @JoinColumn(name="user_id")
+    private User user;
 
     private Address address;
 
@@ -28,6 +31,75 @@ public class Order implements Serializable {
     @Column(name="distribute_type")
     private int distributeType;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public double getTotalMoney() {
+        return totalMoney;
+    }
+
+    public void setTotalMoney(double totalMoney) {
+        this.totalMoney = totalMoney;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public Timestamp getOrderTime() {
+        return orderTime;
+    }
+
+    public void setOrderTime(Timestamp orderTime) {
+        this.orderTime = orderTime;
+    }
+
+    public Timestamp getDistributeTime() {
+        return distributeTime;
+    }
+
+    public void setDistributeTime(Timestamp distributeTime) {
+        this.distributeTime = distributeTime;
+    }
+
+    public int getDistributeType() {
+        return distributeType;
+    }
+
+    public void setDistributeType(int distributeType) {
+        this.distributeType = distributeType;
+    }
 }
